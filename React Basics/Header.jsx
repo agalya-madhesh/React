cebeb44f21c4b5112e6e41b3b8fc836e9d227bac
Agalya <157816927+agalya-madhesh@users.noreply.github.com>
2024-03-21 14:32:39 +0530

    Add files via upload

---
+++ React Basics/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+ 
+    return(
+        <header>
+            <h1>My Website</h1>
+
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">CONTACT</a></li>
+            </ul>
+        </nav>
+        <hr></hr>
+        </header>
+       
+    );
+}
+
+export default Header
