4254d8a30edf1190c5e04c1b785e3622e8563f96
Agalya <157816927+agalya-madhesh@users.noreply.github.com>
2024-04-04 14:31:58 +0530

    Add files via upload

--- Shopping Cart/App.jsx
+++ Shopping Cart/App.jsx
@@ -3,16 +3,18 @@ import './App.css'
 import { Header } from './Components/Header'
 import { Home } from './Components/Home'
 import { Cart } from './Components/Cart'
+import { useState } from 'react'
 
 function App() {
+  const [cart, setCart] = useState([]);
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header cart={cart}/>
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/Cart" element={<Cart/>}/>
+            <Route path="/" element={<Home cart={cart} setCart={setCart}/>}/>
+            <Route path="/Cart" element={<Cart cart={cart} setCart={setCart}/>}/>
           </Routes>
         </div>
       </BrowserRouter>
