10679627a2d19b9b731754e738fdf65f28714fd5
Agalya <157816927+agalya-madhesh@users.noreply.github.com>
2024-04-10 18:25:20 +0530

    Add files via upload

---
+++ Image Slider/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { ImageSlider } from './Components/ImageSlider';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageSlider/>
+    </div>
+  );
+}
+
+export default App;
