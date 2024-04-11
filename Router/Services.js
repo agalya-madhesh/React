ac6e5923b3a9d0135fda7f89694490c2a192b4a9
Agalya <157816927+agalya-madhesh@users.noreply.github.com>
2024-04-11 15:02:22 +0530

    Add files via upload

---
+++ Router/Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Services() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <ul>
+        <li>Service 1</li>
+        <li>Service 2</li>
+        <li>Service 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
