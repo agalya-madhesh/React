c839d06816d5c40089f9b03037adb890d8cadd22
Agalya <157816927+agalya-madhesh@users.noreply.github.com>
2024-05-03 15:26:18 +0530

    Add files via upload

---
+++ Ecommerce Website/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Components/Header";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
